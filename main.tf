725d3699d280086397ef4b6578449b5353bfba2e
nodadyoushutup <admin@nodadyoushutup.com>
2025-08-19 03:19:38 +0000

    yaml from tfvars

--- main.tf
+++ main.tf
@@ -1,77 +1,76 @@
-# # IMAGE
-# resource "proxmox_virtual_environment_download_file" "jammy_cloud_image_amd64_0_1_98" {
-#     content_type = "iso"
-#     datastore_id = "local"
-#     file_name = "jammy-cloud-image-amd64-0.1.98.img"
-#     node_name = "pve"
-#     overwrite = true
-#     overwrite_unmanaged = true
-#     url = "https://cir.nodadyoushutup.com/public/jammy-cloud-image-amd64-0.1.98.img"
-# }
+# IMAGE
+resource "proxmox_virtual_environment_download_file" "jammy_cloud_image_amd64_0_1_98" {
+    content_type = "iso"
+    datastore_id = "local"
+    file_name = "jammy-cloud-image-amd64-0.1.98.img"
+    node_name = "pve"
+    overwrite = true
+    overwrite_unmanaged = true
+    url = "https://cir.nodadyoushutup.com/public/jammy-cloud-image-amd64-0.1.98.img"
+}
 
-# resource "proxmox_virtual_environment_file" "debug_user_cloud_config" {
-#     content_type = "snippets"
-#     datastore_id = "local"
-#     node_name = "pve"
-#     overwrite = true
-#     source_raw {
-#         data = templatefile("${path.module}/cloud_init/debug_user_cloud_config.yaml", {
-#             password = var.password
-#         })
-#         file_name = "debug_user_cloud_config.yaml"
-#     }
-# }
+resource "proxmox_virtual_environment_file" "user_cloud_config" {
+    for_each = { for cloud_init in var.cloud_init : cloud_init.file_name => cloud_init }
+    content_type = each.value.content_type
+    datastore_id = each.value.datastore_id
+    node_name = each.value.node_name
+    overwrite = each.value.overwrite
+    source_raw {
+        data = "#cloud-config\n${yamlencode(each.value.config)}"
+        file_name = each.value.file_name
+    }
+}
 
-# resource "proxmox_virtual_environment_vm" "virtual_machine" {
-#   for_each = { for vm in var.virtual_machine : vm.vm_id => vm }
+resource "proxmox_virtual_environment_vm" "virtual_machine" {
+    for_each = { for vm in var.virtual_machine : vm.vm_id => vm }
 
-#   name = each.value.name
-#   node_name = each.value.node_name
-#   vm_id = each.value.vm_id
-#   bios = each.value.bios
+    name = each.value.name
+    node_name = each.value.node_name
+    vm_id = each.value.vm_id
+    bios = each.value.bios
 
-#   agent {
-#     enabled = each.value.agent.enabled
-#   }
+    agent {
+        enabled = each.value.agent.enabled
+    }
 
-#   cpu {
-#     cores = each.value.cpu.cores
-#   }
+    cpu {
+        cores = each.value.cpu.cores
+    }
 
-#   memory {
-#     dedicated = each.value.memory.dedicated
-#   }
+    memory {
+        dedicated = each.value.memory.dedicated
+    }
 
-#   disk {
-#     datastore_id = each.value.disk.datastore_id
-#     interface = each.value.disk.interface
-#     iothread = each.value.disk.iothread
-#     discard = each.value.disk.discard
-#     size = each.value.disk.size
-#     file_id = each.value.disk.file_id
-#   }
+    disk {
+        datastore_id = each.value.disk.datastore_id
+        interface = each.value.disk.interface
+        iothread = each.value.disk.iothread
+        discard = each.value.disk.discard
+        size = each.value.disk.size
+        file_id = each.value.disk.file_id
+    }
 
-#   initialization {
-#     ip_config {
-#       ipv4 {
-#         address = each.value.initialization.ip_config.ipv4.address
-#         gateway = each.value.initialization.ip_config.ipv4.gateway
-#       }
-#     }
-#     user_data_file_id = each.value.initialization.user_data_file_id
-#   }
+    initialization {
+        ip_config {
+        ipv4 {
+            address = each.value.initialization.ip_config.ipv4.address
+            gateway = each.value.initialization.ip_config.ipv4.gateway
+        }
+        }
+        user_data_file_id = each.value.initialization.user_data_file_id
+    }
 
-#   network_device {
-#     bridge = each.value.network_device.bridge
-#   }
+    network_device {
+        bridge = each.value.network_device.bridge
+    }
 
-#   depends_on = [
-#     proxmox_virtual_environment_download_file.jammy_cloud_image_amd64_0_1_98,
-#     proxmox_virtual_environment_file.debug_user_cloud_config,
-#   ]
-# }
+    depends_on = [
+        proxmox_virtual_environment_download_file.jammy_cloud_image_amd64_0_1_98,
+        proxmox_virtual_environment_file.user_cloud_config,
+    ]
+}
 
 
 output "debug" {
-    value = yamlencode(var.cloud_init.user)
+    value = yamlencode(var.cloud_init[0].config)
 }
--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "endpoint" {
   type        = string
   description = "Proxmox API endpoint, e.g. https://192.168.1.10:8006/"
   validation {
-    condition     = can(regex("^https?://", var.endpoint))
+    condition = can(regex("^https?://", var.endpoint))
     error_message = "endpoint must start with http:// or https://."
   }
 }
@@ -35,11 +35,88 @@ variable "ssh" {
 }
 
 variable "cloud_init" {
-  description = "Cloud init definitions."
-  type = any
+  description = "Cloud-init configuration snippets"
+  type = list(object({
+    content_type = string
+    datastore_id = string
+    node_name    = string
+    overwrite    = optional(bool, false)
+    file_name    = string
+    config = optional(object({
+      hostname = optional(string)
+      timezone = optional(string)
+      mounts   = optional(list(list(string)))
+      groups   = optional(list(string))
+      users = optional(list(object({
+        name                = string
+        groups              = optional(list(string))
+        shell               = optional(string)
+        plain_text_passwd   = optional(string)
+        sudo                = optional(string)
+        lock_passwd         = optional(bool)
+        ssh_import_id       = optional(list(string))
+        ssh_authorized_keys = optional(list(string))
+      })))
+      package_update  = optional(bool)
+      package_upgrade = optional(bool)
+      packages        = optional(list(string))
+      bootcmd         = optional(list(string))
+      runcmd          = optional(list(string))
+    }))
+  }))
 }
 
 variable "virtual_machine" {
-  description = "List of VM definitions."
-  type = any
+  description = "Virtual machine definitions"
+  type = list(object({
+    name      = string
+    node_name = string
+    agent = optional(object({
+      enabled = optional(bool)
+    }))
+    bios = optional(string)
+
+    cpu = optional(object({
+      cores   = optional(number)
+      sockets = optional(number)
+      type    = optional(string)
+    }))
+
+    memory = optional(object({
+      dedicated = optional(number)
+      floating  = optional(number)
+    }))
+
+    disk = optional(object({
+      datastore_id = optional(string)
+      interface    = optional(string)
+      iothread     = optional(bool)
+      discard      = optional(string)
+      size         = optional(number)
+      file_id      = optional(string)
+    }))
+
+    initialization = optional(object({
+      ip_config = optional(object({
+        ipv4 = optional(object({
+          address = optional(string)
+          gateway = optional(string)
+        }))
+        ipv6 = optional(object({
+          address = optional(string)
+          gateway = optional(string)
+        }))
+      }))
+      user_data_file_id = optional(string)
+    }))
+
+    network_device = optional(object({
+      bridge   = optional(string)
+      model    = optional(string)
+      firewall = optional(bool)
+      tag      = optional(number)
+    }))
+
+    vm_id = number
+  }))
 }
