1ccacec330ee7f463f40bc4d0e2b67216a9eb61a
nodadyoushutup <admin@nodadyoushutup.com>
2025-08-19 23:08:23 +0000

    comment output

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "debug" {
-  value = proxmox_virtual_environment_file.monitoring_cloud_config
-}
+# output "debug" {
+#   value = proxmox_virtual_environment_file.monitoring_cloud_config
+# }
